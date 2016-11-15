12d743dfab0dc9cc033d9719e14f28c39b3b8e36
Andrew Kreps <andrew.kreps@gmail.com>
2016-11-14 22:40:41 -0800

    Updates to geo and timestamp to remove slack specific junk and make it work.

--- geo/index.js
+++ geo/index.js
@@ -10,51 +10,28 @@ exports.handler = function(event, context) {
         formatter: null         // 'gpx', 'string', ...
     };
 
-    console.log(event);
+    console.log(event.queryStringParameters);
     var geocoder = NodeGeocoder(geocoderOptions);
-    geocoder.geocode(event.queryStringParameters.text, function(err, res) {
+    var location = event.queryStringParameters.location;
+    geocoder.geocode(location, function(err, res) {
         if (err || res.length == 0) {
             console.log("Error block");
             console.log(err);
-            var response = {
-                response_type: 'in_channel',
-                text: "Unknown place " + event.queryStringParameters.text
-            };
-            superagent
-                .post(event.queryStringParameters.response_url)
-                .send(response)
-                .set('Content-type', 'application/json')
-                .end(function(err, res) {
-                    console.log("Posted successfully!");
-                    context.fail({
-                        statusCode: 200,
-                        headers: {},
-                        body: "Unknown place " + event.queryStringParameters.text
-                    });
-                });
-
+                // text: "Unknown place " + location
         } else {
             console.log("Success block");
             console.log(res);
-
-            var response = {
-                response_type: 'in_channel',
-                text: res[0].latitude.toFixed(7) + ", " + res[0].longitude.toFixed(7) + "\n"
-                + res[0].formattedAddress
-            };
+            response = {
+                lat: res[0].latitude.toFixed(7),
+                lng: res[0].longitude.toFixed(7),
+                address: res[0].formattedAddress
+            }
             console.log("Returning place: " + JSON.stringify(response));
-            superagent
-                .post(event.queryStringParameters.response_url)
-                .send(response)
-                .set('Content-type', 'application/json')
-                .end(function(err, res) {
-                    console.log("Posted successfully!");
-                    context.succeed({
-                        statusCode: 200,
-                        headers: {},
-                        body: JSON.stringify(response)
-                    });
-                });
+            context.succeed({
+                statusCode: 200,
+                headers: {},
+                body: JSON.stringify(response)
+            });
         }
     });
 };
--- timezone/index.js
+++ timezone/index.js
@@ -1,11 +1,11 @@
 var superagent = require('superagent');
 
 exports.handler = function(event, context) {
-    console.log(event);
+    console.log(event.queryStringParameters);
     var timestamp = Math.floor(Date.now() / 1000);
     var uri = 'https://maps.googleapis.com/maps/api/timezone/json?';
 
-    uri += 'location=45,-122';
+    uri += 'location=' + event.queryStringParameters.lat + ',' + event.queryStringParameters.lng;
     uri += '&timestamp=' + timestamp;
     uri += '&key=AIzaSyAlTbxqcZOlb3M-QXR4PCYpS2U1rfgwSlU';
 
@@ -14,11 +14,21 @@ exports.handler = function(event, context) {
     superagent
         .get(uri)
         .end(function(err, res) {
-            console.log(res.body);
-            context.succeed({
-                statusCode: 200,
-                headers: {},
-                body: res.body.rawOffset / 3600
-            });
+            console.log("Body: " + JSON.stringify(res.body));
+            if (res.body.status == "ZERO_RESULTS") {
+                context.fail(JSON.stringify({
+                    status: 400,
+                    errors: [{
+                        code: "400",
+                        message: "Timezone for location not found."
+                    }]
+                }));
+            } else {
+                context.succeed({
+                    statusCode: 200,
+                    headers: {},
+                    body: res.body.rawOffset / 3600
+                });
+            }
         });
 };
